ca9035e9c354d0170ba48f45d347a4b26afc813a
Rojoin <nachoarrastua@gmail.com>
2022-11-23 22:20:52 -0300

    Add Ray

--- Assets/PlayerCamera.cs
+++ Assets/PlayerCamera.cs
@@ -1,5 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditor;
 using UnityEngine;
 
 public class PlayerCamera : MonoBehaviour
@@ -8,23 +10,58 @@ public class PlayerCamera : MonoBehaviour
 
     private Transform player;
     private Rigidbody2D rb;
+    public GameObject currentRoom;
+    public Vector3 example;
+    struct Line
+    {
+        public Vector3 ini;
+        public Vector3 end;
+    }
+
+    Vector3 intersetLine(Plane plane, Vector3 a, Vector3 b)
+    {
+        Vector3 ba = b - a;
+        float nDotA = Vector3.Dot(plane.normal, a);
+        float nDotBA = Vector3.Dot(plane.normal, ba);
+
+        return a + (((plane.distance - nDotA) / nDotBA) * ba);
+    }
+    private Line fline;
 
     private float xRotation = 0f;
   
     void Start()
     {
+    
         Cursor.lockState = CursorLockMode.Locked;
         player = transform.parent;
         rb = GetComponent<Rigidbody2D>();
+        fline.ini = transform.position;
+        fline.end = transform.position + transform.forward * 10;
     }
+
     void Update()
     {
+
+        fline.ini = transform.position;
         var mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
         var mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
         xRotation -= mouseY;
         xRotation = Mathf.Clamp(xRotation, -90f, 90f);
         transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
         player.Rotate(Vector3.up * mouseX);
+        fline.ini = transform.position;
+        fline.end = transform.position + transform.forward * 10;
+        example = intersetLine(currentRoom.GetComponent<Room>().roomPlanes[1], fline.ini, fline.end);
+        if (Mathf.Sign(Vector3.Dot(example, transform.forward)) == 1)
+        {
+            Debug.Log("Puede ser pa");
+        }
+    }
 
+    void OnDrawGizmos()
+    {
+        Gizmos.color =Color.red;
+        Gizmos.DrawLine(fline.ini,fline.end);
     }
 }
--- Assets/Room.cs
+++ Assets/Room.cs
@@ -8,13 +8,21 @@ public class Room : MonoBehaviour
     // Start is called before the first frame update
 
     private Ray rightRoomRaycast;
-    private Ray downRoomRaycast ;
+    private Ray downRoomRaycast;
     public Transform rightPos;
     public Transform downPos;
     public int RoomId;
 
+    [SerializeField] Transform[] planesPos;
+
+   public Plane[] roomPlanes = new Plane[4];
     [SerializeField] GameObject rightRoom;
     [SerializeField] GameObject downRoom;
+
+    public Vector3 plane;
+    public Vector3 plane1;
+    public Vector3 plane2;
+    public Vector3 plane3;
     void Start()
     {
         rightRoomRaycast.origin = rightPos.position;
@@ -26,7 +34,7 @@ public class Room : MonoBehaviour
         RaycastHit down;
         Physics.Raycast(rightRoomRaycast, out right);
         Physics.Raycast(downRoomRaycast, out down);
-        if (right.transform!= null)
+        if (right.transform != null)
         {
             rightRoom = right.transform.gameObject;
 
@@ -36,7 +44,11 @@ public class Room : MonoBehaviour
         {
             downRoom = down.transform.gameObject;
         }
-        
+
+        for (int i = 0; i < roomPlanes.Length; i++)
+        {
+            roomPlanes[i] = new Plane(planesPos[i].up, planesPos[i].position);
+        }
     }
 
     // Update is called once per frame
@@ -46,6 +58,11 @@ public class Room : MonoBehaviour
         rightRoomRaycast.direction = transform.right;
         downRoomRaycast.origin = downPos.position;
         downRoomRaycast.direction = transform.forward * -1;
+
+        plane = roomPlanes[0].normal;
+        plane1 = roomPlanes[1].normal;
+        plane2 = roomPlanes[2].normal;
+        plane3 = roomPlanes[3].normal;
     }
 
 
